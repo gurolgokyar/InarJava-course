60989814bcd92e162b7cd0d11b9f2d513461006e
gurolgokyar <gurolgokyar@gmail.com>
2023-08-09 17:08:22 +0300

    modified Week 01/question 01

--- src/Week_01/Question_01_01.java
+++ src/Week_01/Question_01_01.java
@@ -3,7 +3,7 @@ package Week_01;
 public class Question_01_01 {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java !");
         System.out.println("Welcome to computer Science");
         System.out.println("Programing is fan");
     }
