dd75a7abc6bf6e1aca15ae350122bd98bd27017f
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-10-22 00:46:47 +0300

    Added new questions' solutions

--- src/week_08/assignments/Question_08_15.java
+++ src/week_08/assignments/Question_08_15.java
@@ -6,6 +6,7 @@ public class Question_08_15 {
     public static void main(String[] args) {
         double[][] points = getPoints();
         boolean isSameLine = sameLine(points);
+        System.out.println("The five points are " + (isSameLine ? "" : "not ") + "on the same line");
     }
 
     public static double[][] getPoints(){
@@ -16,20 +17,20 @@ public class Question_08_15 {
 
         for (int i = 0; i < points.length; i++) {
             for (int j = 0; j < points[0].length; j++) {
-                points[1][j] = input.nextDouble();
+                points[i][j] = input.nextDouble();
             }
         }
         return points;
     }
 
     public static boolean sameLine(double[][] points){
-        for (int i = 2; i < points.length -1; i++) {
+        for (int i = 2; i < points.length; i++) {
                 double status =
-                        (points[1][0] - points[0][0]) * (points[i][1] - points[0][1]) -  (points[i][0] - points[0][0]) * (points[i][1] - points[0][1]);
-            if (status == 0) {
-                return 
+                        (points[1][0] - points[0][0]) * (points[i][1] - points[0][1]) -  (points[i][0] - points[0][0]) * (points[1][1] - points[0][1]);
+            if (status != 0) {
+                return false;
             }
-
         }
+        return true;
     }
 }
