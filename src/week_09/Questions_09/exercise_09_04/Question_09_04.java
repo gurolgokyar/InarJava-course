3e43aa7725f2ba65a4b1d47ef676487cf234a77e
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-10-29 20:05:43 +0300

    Added new questions' solutions

---
+++ src/week_09/Questions_09/exercise_09_04/Question_09_04.java
@@ -0,0 +1,15 @@
+package week_09.Questions_09.exercise_09_04;
+
+import java.util.Random;
+
+public class Question_09_04 {
+    public static void main(String[] args) {
+        Random random = new Random(1000);
+        for (int i = 0; i < 50; i++) {
+            System.out.print(random.nextInt(100) + " ");
+            if ((i + 1) % 10 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
