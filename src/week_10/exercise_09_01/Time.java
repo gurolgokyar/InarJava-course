2facc198fe1568ad86dcfaa30f5b633c6be5eecc
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-11-03 00:00:17 +0300

    Modified /week_10/exercise_09_01/Time.java

--- src/week_10/exercise_09_01/Time.java
+++ src/week_10/exercise_09_01/Time.java
@@ -5,6 +5,16 @@ public class Time {
     private int minute;
     private int second;
     Time(){
+        long milliseconds = System.currentTimeMillis();
+        this.hour = getHour(milliseconds);
+        this.minute = getMinuts(milliseconds);
+        this.getSecond = getSeconds(milliseconds);
 
+    }
+    public int getHour(long milliseconds){
+        return (int)(getTotalHour(milliseconds) % 24);
+    }
+    public long getTotalHour(long milliseconds){
+        return getTotalMinutes(milliseconds) / 60;
     }
 }
