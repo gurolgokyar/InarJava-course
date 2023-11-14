4e31d36e3e393f388e51cfc8f9f6d4ebd444d6b6
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-11-14 22:03:40 +0300

    Added new questions' solutions

--- src/week_11_objectOrientedPrograming_3/working_area/TestClass.java
+++ src/week_11_objectOrientedPrograming_3/working_area/TestClass.java
@@ -21,22 +21,33 @@ public class TestClass {
 }
 
 class A extends TestClass {
-    public A() {
+    String s;
+   // public A() {
         // If there is not a no-argument constructor in the super class you must explicitly write the constructor of
         // the super class in the sub-class like blow
         //super("Calm dawn!");
-        System.out.println("Hello world 3");
-    }
+//        s = "";
+//        System.out.println("Hello world 3");
+  //  }
 
     public A(String s) {
+        this.s = s;
         //super("Calm dawn 2!");
-        System.out.println("Hello world 4");
+        //System.out.println("Hello world 4");
+    }
+}
+class C extends TestClass{
+    String s;
+    public C(String s){
+        this.s = s;
     }
 }
 
 class B {
     public static void main(String[] args) {
-       A a = new A();
-       A a1 = new A("Hey");
+        System.out.println(new A("Key").equals(new A("Key")));
+
+//       A a = new A();
+//       A a1 = new A("Hey");
     }
 }
