f460138c41bba9e40a57372a06a95fc2d88960a4
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-11-16 00:06:53 +0300

    Added new qoestions' solutions

---
+++ src/week_11_objectOrientedPrograming_3/assignments/exercise_11_02/Employee.java
@@ -0,0 +1,51 @@
+package week_11_objectOrientedPrograming_3.assignments.exercise_11_02;
+
+import week_10_objectOrientedProgramind_2.exercise_10_14.MyDate;
+
+public class Employee extends Person{
+    private int office;
+    private double salary;
+    private MyDate dateHired = new MyDate();
+    public Employee(){
+
+    }
+    public Employee(String name, String address, String phoneNumber, String emailAddress, int office, double salary){
+        super(name, address, phoneNumber, emailAddress);
+        this.office = office;
+        this.salary = salary;
+    }
+
+    public int getOffice() {
+        return office;
+    }
+
+    public void setOffice(int office) {
+        this.office = office;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public MyDate getDateHired() {
+        return dateHired;
+    }
+
+    public void setDateHired(MyDate dateHired) {
+        this.dateHired = dateHired;
+    }
+    public String toString(){
+        return "  _Employee_" +
+                "\nName: " + getName() +
+                "\nAddress: " + getAddress() +
+                "\nPhone number: " + getPhoneNumber() +
+                "\nEmail address: " + getEmailAddress() +
+                "\nOffice: " + getOffice() +
+                "\nSalary: " + getSalary() +
+                "\nHiredDate: " + dateHired.toString();
+    }
+}
---
+++ src/week_11_objectOrientedPrograming_3/assignments/exercise_11_02/Student.java
@@ -0,0 +1,31 @@
+package week_11_objectOrientedPrograming_3.assignments.exercise_11_02;
+
+public class Student extends Person {
+    public static final int freshman = 0;
+    public static final int sophomore = 1;
+    public static final int junior = 2;
+    public static final int senior = 3;
+    private int status;
+
+    public Student(String name, String address, String phoneNumber, String emailAddress, int status) {
+        super(name, address, phoneNumber, emailAddress);
+       this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        return "  _Student_" +
+                "\nName: " + getName() +
+                "\nAddress: " + getAddress() +
+                "\nPhone number: " + getPhoneNumber() +
+                "\nEmail address: " + getEmailAddress() +
+                "\nClassStatus: " + status;
+    }
+}
