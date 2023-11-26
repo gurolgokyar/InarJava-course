be65bf95a381c58f6d97437e5f6db7fbbe128db3
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-11-27 01:08:37 +0300

    deleted:    week_12_exceptionHandling/assignments/exercise_12_32/Qued.java

--- src/week_12_exceptionHandling/assignments/exercise_12_32/jkşdfg.java
+++
@@ -1,8 +0,0 @@
-package week_12_exceptionHandling.assignments.exercise_12_32;
-
-public class jkşdfg {
-    public static void main(String[] args) {
-        String str = "alixcvsfgnali";
-        System.out.println(str.indexOf("ali",1));
-    }
-}
